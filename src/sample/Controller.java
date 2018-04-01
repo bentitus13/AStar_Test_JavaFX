b0f700bbf08c277ff6d5f4f49e94c95925aa44be
Ben Titus <ben.titus13@gmail.com>
2018-04-01 18:16:35 -0400

    made nodes auto generate. Functionality still the same

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,13 +2,20 @@ package sample;
 
 import AStar.Edge;
 import AStar.Node;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.StrokeType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,13 +27,20 @@ import java.util.Iterator;
 public class Controller {
     private boolean firstSelect = true;
     private boolean secondSelect = true;
-    private Node startNode;
-    private Node endNode;
+    private static Node startNode;
+    private static Node endNode;
 
     @FXML
     private Canvas testCanvas;
+
+    @FXML
     private Button goButton;
+
+    @FXML
     private Label label;
+
+    @FXML
+    private AnchorPane anchorPane;
 
     private static Node Node01 = new Node(50.0,50.0);
     private static Node Node02 = new Node(100.0, 50.0);
@@ -64,85 +78,67 @@ public class Controller {
     static ArrayList<Edge> edgelist09 = new ArrayList<Edge>();
     static ArrayList<Edge> edgelist10 = new ArrayList<Edge>();
 
-    HashMap<String, Node> nodeSet = new HashMap<String, Node>();
-    HashMap<String, Edge> edgeSet = new HashMap<String, Edge>();
+    static ArrayList<Node> nodelist = new ArrayList<Node>();
 
-    HashMap<String, Circle> nodeDispSet = new HashMap<String, Circle>();
-    HashMap<String, Line> edgeDispSet = new HashMap<String, Line>();
+    static HashMap<String, Node> nodeSet = new HashMap<String, Node>();
+    static HashMap<String, Edge> edgeSet = new HashMap<String, Edge>();
 
-    @FXML
-    private Circle node01;
+    static HashMap<String, Circle> nodeDispSet = new HashMap<String, Circle>();
+    static HashMap<String, Line> edgeDispSet = new HashMap<String, Line>();
 
-    @FXML
-    private Circle node02;
+    EventHandler<MouseEvent> clickHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            for (String string : nodeDispSet.keySet()) {
+                if (nodeDispSet.get(string) == event.getSource()) {
+                    Node node = nodeSet.get(string);
+                    if (firstSelect) {
+                        nodeDispSet.get(node.getID()).setFill(Color.rgb(35, 205, 0, .99));
+                        startNode = node;
+                        firstSelect = false;
+                    } else if (secondSelect) {
+                        nodeDispSet.get(node.getID()).setFill(Color.rgb(205, 35, 0, 0.99));
+                        endNode = node;
+                        secondSelect = false;
+                    }
+                }
+            }
+            event.consume();
+        }
+    };
 
-    @FXML
-    private Circle node03;
+    public void drawNodes() {
+        for (Node node : nodelist) {
+            Circle circle = new Circle(5.0);
+            circle.setCenterX(node.getX());
+            circle.setCenterY(node.getY());
+            circle.setFill(Color.DODGERBLUE);
+            circle.setStroke(Color.BLACK);
+            circle.setStrokeType(StrokeType.INSIDE);
+            circle.setOnMouseClicked(clickHandler);
 
-    @FXML
-    private Circle node04;
+            String label = node.getID();
+            nodeDispSet.put(label, circle);
+        }
+    }
 
-    @FXML
-    private Circle node05;
+    public void drawEdges() {
+        for (Edge edge : edgeSet.values()) {
+            Line line = new Line();
+            line.setStartX(edge.getNode1().getX());
+            line.setStartY(edge.getNode1().getY());
+            line.setEndX(edge.getNode2().getX());
+            line.setEndY(edge.getNode2().getY());
+            line.setStrokeWidth(1.0);
+            line.setStrokeType(StrokeType.CENTERED);
 
-    @FXML
-    private Circle node06;
-
-    @FXML
-    private Circle node07;
-
-    @FXML
-    private Circle node08;
-
-    @FXML
-    private Circle node09;
-
-    @FXML
-    private Circle node10;
-
-    @FXML
-    private Line edge0102;
-
-    @FXML
-    private Line edge0104;
-
-    @FXML
-    private Line edge0203;
-
-    @FXML
-    private Line edge0304;
-
-    @FXML
-    private Line edge0305;
-
-    @FXML
-    private Line edge0309;
-
-    @FXML
-    private Line edge0409;
-
-    @FXML
-    private Line edge0506;
-
-    @FXML
-    private Line edge0508;
-
-    @FXML
-    private Line edge0607;
-
-    @FXML
-    private Line edge0610;
-
-    @FXML
-    private Line edge0708;
-
-    @FXML
-    private Line edge0809;
-
-    @FXML
-    private Line edge0910;
+            String label = edge.getID();
+            edgeDispSet.put(label, line);
+        }
+    }
 
     public void setEdgeLists() {
+
         edgelist01.add(Edge0102);
         edgelist01.add(Edge0104);
         edgelist02.add(Edge0102);
@@ -208,6 +204,17 @@ public class Controller {
         Edge0708.setID("Edge 7-8");
         Edge0809.setID("Edge 8-9");
         Edge0910.setID("Edge 9-10");
+
+        nodelist.add(Node01);
+        nodelist.add(Node02);
+        nodelist.add(Node03);
+        nodelist.add(Node04);
+        nodelist.add(Node05);
+        nodelist.add(Node06);
+        nodelist.add(Node07);
+        nodelist.add(Node08);
+        nodelist.add(Node09);
+        nodelist.add(Node10);
     }
 
     public void setHashSets() {
@@ -236,190 +243,22 @@ public class Controller {
         edgeSet.put(Edge0708.getID(), Edge0708);
         edgeSet.put(Edge0809.getID(), Edge0809);
         edgeSet.put(Edge0910.getID(), Edge0910);
-
-
-        nodeDispSet.put(Node01.getID(), node01);
-        nodeDispSet.put(Node02.getID(), node02);
-        nodeDispSet.put(Node03.getID(), node03);
-        nodeDispSet.put(Node04.getID(), node04);
-        nodeDispSet.put(Node05.getID(), node05);
-        nodeDispSet.put(Node06.getID(), node06);
-        nodeDispSet.put(Node07.getID(), node07);
-        nodeDispSet.put(Node08.getID(), node08);
-        nodeDispSet.put(Node09.getID(), node09);
-        nodeDispSet.put(Node10.getID(), node10);
-
-        edgeDispSet.put(Edge0102.getID(), edge0102);
-        edgeDispSet.put(Edge0104.getID(), edge0104);
-        edgeDispSet.put(Edge0203.getID(), edge0203);
-        edgeDispSet.put(Edge0304.getID(), edge0304);
-        edgeDispSet.put(Edge0305.getID(), edge0305);
-        edgeDispSet.put(Edge0309.getID(), edge0309);
-        edgeDispSet.put(Edge0409.getID(), edge0409);
-        edgeDispSet.put(Edge0506.getID(), edge0506);
-        edgeDispSet.put(Edge0508.getID(), edge0508);
-        edgeDispSet.put(Edge0607.getID(), edge0607);
-        edgeDispSet.put(Edge0610.getID(), edge0610);
-        edgeDispSet.put(Edge0708.getID(), edge0708);
-        edgeDispSet.put(Edge0809.getID(), edge0809);
-        edgeDispSet.put(Edge0910.getID(), edge0910);
-    }
-
-    @FXML
-    public void setNode01() {
-        if (firstSelect) {
-            firstSelect = false;
-            node01.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node01;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node01.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node01;
-        }
-    }
-
-    @FXML
-    public void setNode02() {
-        if (firstSelect) {
-            firstSelect = false;
-            node02.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node02;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node02.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node02;
-        }
-    }
-
-    @FXML
-    public void setNode03() {
-        if (firstSelect) {
-            firstSelect = false;
-            node03.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node03;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node03.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node03;
-        }
-    }
-
-    @FXML
-    public void setNode04() {
-        if (firstSelect) {
-            firstSelect = false;
-            node04.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node04;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node04.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node04;
-        }
-    }
-
-    @FXML
-    public void setNode05() {
-        if (firstSelect) {
-            firstSelect = false;
-            node05.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node05;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node05.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node05;
-        }
-    }
-
-    @FXML
-    public void setNode06() {
-        if (firstSelect) {
-            firstSelect = false;
-            node06.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node06;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node06.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node06;
-        }
-    }
-
-    @FXML
-    public void setNode07() {
-        if (firstSelect) {
-            firstSelect = false;
-            node07.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node07;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node07.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node07;
-        }
-    }
-
-    @FXML
-    public void setNode08() {
-        if (firstSelect) {
-            firstSelect = false;
-            node08.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node08;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node08.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node08;
-        }
-    }
-
-    @FXML
-    public void setNode09() {
-        if (firstSelect) {
-            firstSelect = false;
-            node09.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node09;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node09.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node09;
-        }
-    }
-
-    @FXML
-    public void setNode10() {
-        if (firstSelect) {
-            firstSelect = false;
-            node10.setFill(Color.rgb(35, 205, 0, .99));
-            startNode = Node10;
-        } else if (secondSelect) {
-            secondSelect = false;
-            node10.setFill(Color.rgb(205, 35, 0, 0.99));
-            endNode = Node10;
-        }
     }
 
     @FXML
     public void goButtonPressed() {
-        setEdgeLists();
-        setHashSets();
         ArrayList<Node> path = startNode.AStar(endNode);
-//        System.out.println("Final path: " + path.toString());
         Node currentNode = null, pastNode = null;
-//        System.out.println("Current Node: " + currentNode.toString());
         Circle waypoint;
         Line line;
-//        System.out.println("Edges on current node: " + currentNode.getEdges().toString());
         for (Node n : path) {
             pastNode = currentNode;
             currentNode = n;
-//            System.out.println("Current Node: " + currentNode.toString());
-//            System.out.println("Previous Node: " + pastNode.toString());
             waypoint = nodeDispSet.get(currentNode.getID());
             waypoint.setFill(Color.rgb(250, 150, 0));
-//            System.out.println("Edges on current node: " + currentNode.getEdges().toString());
             for (Edge e : currentNode.getEdges()) {
-//                System.out.println("iterator j next element: " + j.hasNext());
-//                System.out.println("Current Edge: " + currentEdge.toString());
                 if (pastNode != null) {
                     if (e.contains(pastNode)) {
-//                        System.out.println("We got a hit!" + currentEdge.toString());
                         line = edgeDispSet.get(e.getID());
                         line.setStroke(Color.rgb(250, 150, 0));
                         line.setStrokeWidth(2.0);
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,20 +3,51 @@ package sample;
 import AStar.Edge;
 import AStar.Node;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main extends Application {
+    Controller controller = new Controller();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        controller.setEdgeLists();
+        controller.setHashSets();
+
+        Group group = new Group();
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        group.getChildren().addAll(root);
+
+        // Add edges to UI
+        controller.drawEdges();
+        HashMap<String, Line> lineList = controller.edgeDispSet;
+        for (Line line : lineList.values()) {
+            group.getChildren().add(line);
+        }
+
+        // Add nodes to UI
+        controller.drawNodes();
+        HashMap<String, Circle> circleList = controller.nodeDispSet;
+        for (Circle c : circleList.values()) {
+            group.getChildren().add(c);
+        }
+
         primaryStage.setTitle("Pathfinding Test");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setScene(new Scene(group, 600, 600));
         primaryStage.show();
     }
 
